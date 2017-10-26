ffcf39f18592e3a0a95c1056c8623b6c3e46ab94
Serge Ovt <osamsk@gmail.com>
2017-10-26 10:11:51 +0300

    HW_6_edit_2.1

--- src/ru/javawebinar/basejava/storage/MapResumeStorage.java
+++ src/ru/javawebinar/basejava/storage/MapResumeStorage.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MapResumeStarage extends AbstractStorage{
+public class MapResumeStorage extends AbstractStorage{
 
     private Map<String, Resume> map = new HashMap<>();
 
