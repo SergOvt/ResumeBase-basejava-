3eb379e3eaee601adac4c15b0350cda00eedc0ef
Serge Ovt <osamsk@gmail.com>
2017-10-26 10:10:55 +0300

    HW_6_edit_2

---
+++ src/ru/javawebinar/basejava/storage/MapResumeStarage.java
@@ -0,0 +1,58 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapResumeStarage extends AbstractStorage{
+
+    private Map<String, Resume> map = new HashMap<>();
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public List<Resume> getList() {
+        return new ArrayList<>(map.values());
+    }
+
+    @Override
+    protected boolean isExist(Object resume) {
+        return resume != null;
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Object resume) {
+        map.put(((Resume)resume).getUuid(), r);
+    }
+
+    @Override
+    public void doSave(Resume r, Object resume) {
+        map.put(((Resume)resume).getUuid(), r);
+    }
+
+    @Override
+    public void doDelete(Object resume) {
+        map.remove(((Resume)resume).getUuid());
+    }
+
+    @Override
+    public Resume doGet(Object resume) {
+        return (Resume) resume;
+    }
+
+    @Override
+    protected Resume getSearchKey (String uuid) {
+        return map.get(uuid);
+    }
+}
