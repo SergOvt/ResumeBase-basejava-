d691baba3c1a9138ac419713cd1baac3292ebcab
Serge Ovt <osamsk@gmail.com>
2017-11-06 11:36:04 +0300

    HW11_edit_1

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -3,37 +3,32 @@ package ru.javawebinar.basejava;
 
 public class MainDeadLock {
 
-    private final Object lock1 = new Object();
-    private final Object lock2 = new Object();
+    private static final Object lock1 = new Object();
+    private static final Object lock2 = new Object();
 
     public static void main(String[] args) throws InterruptedException {
 
-        MainDeadLock dl = new MainDeadLock();
-
-        for (int i = 0; i < 100; i++) {
-            new Thread(() -> {
-                dl.method1();
-                dl.method2();
-            }).start();
-        }
-
+        new Thread(() -> method(lock1, lock2)).start();
+        method(lock2, lock1);
     }
 
-    void method1(){
+    static void method(Object lock1, Object lock2) {
+
         synchronized (lock1) {
+            System.out.println("In first synchro befor sleep");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("In first synchro after sleep");
+
             synchronized (lock2) {
-                System.out.println("Method1");
+                System.out.println("In second synchro");
             }
         }
     }
-
-    void method2(){
-        synchronized (lock2) {
-            synchronized (lock1) {
-                System.out.println("Method2");
-            }
-        }
-    }
-
 
 }
